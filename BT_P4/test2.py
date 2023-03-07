418905d8abf8653797b0820a9d4f75c27bfcf678
Ahn <nha2942004@gmail.com>
2023-03-07 22:16:37 +0700

    Tao mang hai chieu

---
+++ BT_P4/test2.py
@@ -0,0 +1,17 @@
+def nhap(lis):
+    for i in range(x):
+        lis.append([])
+        for j in range(y):
+            lis[i].append(i * j)
+
+
+def xuat(lis):
+    for i in lis:
+        print(i)
+
+
+x = int(input("Nhap so hang: "))
+y = int(input("Nhap so cot: "))
+lis = []
+nhap(lis)
+xuat(lis)
