be3799f6ef6e3ab9e87ead11466800ea67e257f9
Ahn <nha2942004@gmail.com>
2023-03-08 13:37:21 +0700

    Chuyển chuỗi thành chuỗi in hoa

---
+++ BT_P4/test4.py
@@ -0,0 +1,18 @@
+lis = []
+
+while True:
+    s = input()
+    if s:
+        lis.append(s.upper())
+    else:
+        break
+for i in lis:
+    print(i)
+
+#Vidu:
+#Đầu vào:
+#Hello world
+#Practice makes perfect
+#Đầu ra:
+#HELLO WORLD
+#PRACTICE MAKES PERFECT
